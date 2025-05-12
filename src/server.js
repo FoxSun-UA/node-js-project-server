ab799d7ee882f485d125c9aa60818c2835c3116a
FoxSun <foxsun.ua@gmail.com>
2025-05-12 15:47:23 +0300

    code-organization

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import studentRouter from './routers/students.js';
 import { getEnvVar } from './units/getEnvVar.js';
-import { getAllStudents, getStudentById } from './services/students.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+// import { getAllStudents, getStudentById } from './services/students.js';
 
 const PORT = Number(getEnvVar('PORT', 3500));
 
@@ -26,47 +29,61 @@ export const startServer = () => {
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
+  app.use(studentRouter);
 
-    res.status(200).json({
-      data: students,
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.get('/students/:studentId', async (req, res, next) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      data: student,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
   });
 };
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world!',
+//   });
+// });
+
+// app.get('/students', async (req, res) => {
+//   const students = await getAllStudents();
+
+//   res.status(200).json({
+//     data: students,
+//   });
+// });
+
+// app.get('/students/:studentId', async (req, res, next) => {
+//   const { studentId } = req.params;
+//   const student = await getStudentById(studentId);
+
+//    Відповідь, якщо контакт не знайдено
+
+//   if (!student) {
+//     res.status(404).json({
+//       message: 'Student not found',
+//     });
+//     return;
+//   }
+
+//    Відповідь, якщо контакт знайдено
+
+//   res.status(200).json({
+//     data: student,
+//   });
+// });
+
+// app.use((req, res, next) => {
+//   res.status(404).json({
+//     message: 'Not found',
+//   });
+// });
+
+// app.use((err, req, res, next) => {
+//   res.status(500).json({
+//     message: 'Something went wrong',
+//     error: err.message,
+//   });
+// });
